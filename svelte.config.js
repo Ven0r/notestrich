3b4a302a7200f32ee19c161e3494f7e761017350
venor <venor.stdout@gmail.com>
2024-12-17 20:01:43 -0600

    Changed Fallback.html Naming

--- svelte.config.js
+++ svelte.config.js
@@ -2,16 +2,16 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
-    kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: '200.html', // Serve 200.html for all non-prerendered routes
-        }),
-        prerender: {
-            entries: ['*'], // Prerender all static pages
-        }
-    },
-    preprocess: vitePreprocess(), // Enable Vite preprocessing
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'fallback.html', // Serve 200.html for all non-prerendered routes
+    }),
+    prerender: {
+      entries: ['*'], // Prerender all static pages
+    }
+  },
+  preprocess: vitePreprocess(), // Enable Vite preprocessing
 };
 
